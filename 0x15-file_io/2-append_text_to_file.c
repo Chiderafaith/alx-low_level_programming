593ae3cc427413ad9f2b4ae7199ac9ee6699ced5
chiderafaith <faithjosephs114@gmail.com>
2022-08-23 15:22:56 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,8 @@ int append_text_to_file(const char *filename, char *text_content)
 int fd;
 ssize_t len;
 if (filename == NULL)
-return (-1);fd = open(filename, O_WRONLY | O_APPEND);
+return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 if (text_content != NULL)
